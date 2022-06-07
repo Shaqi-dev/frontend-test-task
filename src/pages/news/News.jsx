b85a436f5eaa9c136fc399b5a6d088005b9bacf2
Vladislav Zubakin <shaqimusic@gmail.com>
2022-06-08 01:46:09 +0300

    feat: add news search

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { authSelector } from '../../redux/authSlice';
 import { newsSelector } from '../../redux/newsSlice';
+import useDebounce from '../../hooks/useDebounce';
 import { USER, ADMIN } from '../../constants';
 import PageTitle from '../../components/pageTitle/PageTitle';
 import AddNewsArticle from '../../components/addNews/AddNewsArticle';
@@ -11,12 +12,44 @@ import './News.scss';
 function News() {
   const news = useSelector(newsSelector);
   const { userType } = useSelector(authSelector);
+  const searchInputEl = useRef(null);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [newsResults, setNewsResults] = useState(news);
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      const filterResult = news.filter((item) => {
+        const title = item.title.toLowerCase();
+        const description = item.description.toLowerCase();
+        const search = debouncedSearchTerm.toLowerCase();
+
+        return title.indexOf(search) > -1 || description.indexOf(search) > -1;
+      });
+
+      setNewsResults(filterResult);
+    } else {
+      setNewsResults(news);
+    }
+  }, [debouncedSearchTerm]);
+
+  const handleSearch = () => setSearchTerm(searchInputEl.current.value);
+
   return (
     <main className="news">
       <PageTitle className="news__title">Новости</PageTitle>
+      <input
+        ref={searchInputEl}
+        type="text"
+        placeholder="Поиск"
+        className="news__search-input"
+        onChange={handleSearch}
+      />
       <div className="news__list">
         {
-          news.map((item) => (
+          newsResults.map((item) => (
             <NewsArticle
               key={item.id}
               title={item.title}
