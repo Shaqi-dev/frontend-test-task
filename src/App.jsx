2aef3db875559f245eaf4e9d4d5d9f88fdce6e1f
Vladislav Zubakin <shaqimusic@gmail.com>
2022-06-07 04:30:53 +0300

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header';
+import Home from './pages/home';
+import News from './pages/news';
+import NotFound from './pages/notFound';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
