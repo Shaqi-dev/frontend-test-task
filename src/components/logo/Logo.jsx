1b75165d0ffdb81fe5354f8df6b0437215c58139
Vladislav Zubakin <shaqimusic@gmail.com>
2022-06-07 04:32:42 +0300

    feat: add router links

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/svg/logo.svg';
 
 function Logo() {
   return (
-    <div className="logo">
+    <Link to="/" className="logo">
       <img src={logo} alt="Profilance Group Logo" />
-    </div>
+    </Link>
   );
 }
 
--- src/components/nav/navItem/NavItem.jsx
+++ src/components/nav/navItem/NavItem.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './NavItem.scss';
 
 function NavItem({ page }) {
   const { link, title } = page;
   const displayTitle = title.charAt(0).toUpperCase() + title.slice(1);
   return (
-    <a href={link} className="nav__item">
+    <Link to={link} className="nav__item">
       {displayTitle}
-    </a>
+    </Link>
   );
 }
 NavItem.propTypes = {
